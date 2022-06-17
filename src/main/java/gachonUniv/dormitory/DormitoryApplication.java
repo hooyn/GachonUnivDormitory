86fc13d2343e5146c082e96abf120c5e804cd823
hooyn <hooyn_industry@naver.com>
2022-06-17 12:33:12 +0900

    queryFactory Bean 설정

--- src/main/java/gachonUniv/dormitory/DormitoryApplication.java
+++ src/main/java/gachonUniv/dormitory/DormitoryApplication.java
@@ -1,7 +1,11 @@
 package gachonUniv.dormitory;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class DormitoryApplication {
@@ -10,4 +14,8 @@ public class DormitoryApplication {
 		SpringApplication.run(DormitoryApplication.class, args);
 	}
 
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em){
+		return new JPAQueryFactory(em);
+	}
 }
