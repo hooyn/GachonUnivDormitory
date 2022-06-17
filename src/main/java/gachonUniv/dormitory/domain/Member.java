bd2d808d53f35377ef6749eda278cfda321f6319
hooyn <hooyn_industry@naver.com>
2022-06-17 12:33:24 +0900

    Member Domain 개발

---
+++ src/main/java/gachonUniv/dormitory/domain/Member.java
@@ -0,0 +1,48 @@
+package gachonUniv.dormitory.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)", name = "member_id")
+    private UUID id;
+
+    private String userID;
+    private String userPW;
+    private String nickname;
+    private Boolean certified;
+    private String token;
+    private String isAOS;
+
+    /**
+     * [Mapping]
+     * postList
+     * replyList
+     * reportList
+     * feedbackList
+     * notificationList
+     */
+
+    public Member(String userID, String userPW, String nickname, Boolean certified, String token, String isAOS) {
+        this.userID = userID;
+        this.userPW = userPW;
+        this.nickname = nickname;
+        this.certified = certified;
+        this.token = token;
+        this.isAOS = isAOS;
+    }
+}
