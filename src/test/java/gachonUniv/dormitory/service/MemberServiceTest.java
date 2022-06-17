58a4fd0131ea0b96ce4c9c9fcd6426f122707371
hooyn <hooyn_industry@naver.com>
2022-06-17 12:33:59 +0900

    login Test

---
+++ src/test/java/gachonUniv/dormitory/service/MemberServiceTest.java
@@ -0,0 +1,30 @@
+package gachonUniv.dormitory.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    public void login_test() throws Exception {
+        //given
+        String data = memberService.login("ownerchef2", "owner9809~");
+        //when
+        System.out.println("------------------------------------------------");
+        System.out.println("------------------------------------------------");
+        System.out.println(data);
+        System.out.println("------------------------------------------------");
+        System.out.println("------------------------------------------------");
+        //then
+    }
+}
