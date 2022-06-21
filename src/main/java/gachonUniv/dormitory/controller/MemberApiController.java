1ff5bb13a33c43a2656c7dc9f0117651415eccf6
hooyn <hooyn_industry@naver.com>
2022-06-21 14:34:21 +0900

    Update MemberApiController [/login]

--- src/main/java/gachonUniv/dormitory/controller/MemberApiController.java
+++ src/main/java/gachonUniv/dormitory/controller/MemberApiController.java
@@ -1,13 +1,9 @@
 package gachonUniv.dormitory.controller;
 
-import gachonUniv.dormitory.domain.Member;
-import gachonUniv.dormitory.dto.FindMemberDto;
-import gachonUniv.dormitory.request.CreateMemberRequest;
-import gachonUniv.dormitory.request.FindOneByUserIdRequest;
+import gachonUniv.dormitory.request.LoginRequest;
 import gachonUniv.dormitory.response.Response;
 import gachonUniv.dormitory.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,23 +15,28 @@ import java.util.UUID;
 public class MemberApiController {
     private final MemberService memberService;
 
-    @PostMapping("/create_member")
-    public Response createMember(@RequestBody CreateMemberRequest request){
-        Member member = new Member(request.getUserID(), request.getUserPW(), request.getNickname(), request.getCertified(), request.getToken(), request.getIsAOS());
-        UUID data = memberService.createMember(member);
+    @PostMapping("/login")
+    public Response login(@RequestBody LoginRequest request){
 
-        return new Response(true, HttpStatus.OK, "ID = " + data, "회원 한명 조회");
-    }
+        //아이디 검색
+            //certified == true 학교인증 완료
+                //비밀번호 확인
+                    //비밀번호 일치
+                        //닉네임 설정 확인
+                            //닉네임 설정
+                                //return 200
+                            //닉네임 미설정
+                                //return 201 [닉네임 설정]
+                    //비밀번호 불일치
+                        //return 301
 
-    @PostMapping("/member")
-    public Response findOneByUuid(@RequestBody FindOneByUserIdRequest request){
-        Member member = memberService.findMember(request.getUserID());
+            //certified == false 학교인증 미완료
+                //학교인증
+                    //학생 인증 완료
+                        //DB에 저장 certified==1
+                        //닉네임 설정 return 201 [닉네임 설정]
 
-        if(member==null){
-            return new Response(true, HttpStatus.BAD_REQUEST, null, "회원 정보를 찾을 수 없음");
-        }
-
-        FindMemberDto data = new FindMemberDto(member.getId(), member.getUserID(), member.getNickname(), member.getCertified(), member.getToken(), member.getIsAOS());
-        return new Response(true, HttpStatus.OK, data, "회원 한명 조회");
+                    //학생 인증 미완료
+                        //return 301
     }
 }
