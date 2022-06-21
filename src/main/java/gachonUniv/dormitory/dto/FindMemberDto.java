f06dc36601ea5e41fd09bf90a2e10fda18710bb3
hooyn <hooyn_industry@naver.com>
2022-06-21 14:29:32 +0900

    Update FindMemberDto

--- src/main/java/gachonUniv/dormitory/dto/FindMemberDto.java
+++ src/main/java/gachonUniv/dormitory/dto/FindMemberDto.java
@@ -10,14 +10,16 @@ import java.util.UUID;
 public class FindMemberDto {
     private UUID uuid;
     private String userID;
+    private String userPw;
     private String nickname;
     private Boolean certified;
     private String token;
     private Boolean isAOS;
 
-    public FindMemberDto(UUID uuid, String userID, String nickname, Boolean certified, String token, Boolean isAOS) {
+    public FindMemberDto(UUID uuid, String userID, String userPw, String nickname, Boolean certified, String token, Boolean isAOS) {
         this.uuid = uuid;
         this.userID = userID;
+        this.userPw = userPw;
         this.nickname = nickname;
         this.certified = certified;
         this.token = token;
